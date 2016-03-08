3b51b373610eb7e396208c27c52424c8fcb96716
amandasjostrom <amandasjostrom@hotmail.com>
2016-03-08 14:35:14 +0100

    blä

--- IIProjectClient/Controllers/TestController.cs
+++ IIProjectClient/Controllers/TestController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using IIProjectClient.FordonsPassageServiceReference;
 namespace IIProjectClient.Controllers
 {
     public class TestController : Controller
@@ -11,6 +11,32 @@ namespace IIProjectClient.Controllers
         // GET: Test
         public ActionResult Index()
         {
+//            <förfrågan>
+//    <datetimeFörfrågan></datetimeFörfrågan>
+//    <anropsansvarig>
+//        <namn>
+//        <lösenord>
+//</anropsansvarig>
+//    <tidsintervall>
+//        <start></start>
+//        <slut></slut>
+//    </tidsintervall>
+//    <plats></plats>
+//    <paginering>
+//        <antal></antal>
+//        <senastTagna></senastTagna>
+//    </paginering>
+//</förfrågan>
+            FordonsPassageServiceClient client = new FordonsPassageServiceClient();
+            XElement förfrågan = new XElement("Förfrågan",
+                new XElement("tidsintervall",
+                    new XElement("start",  ),
+                    new XElement("slut", )
+                ),
+                new XElement("plats", "urn:epc:id:sgln:735999271.000.13")
+                
+                )
+            XElement svar = client.HämtaFordonsPassager(förfrågan);
             return View();
         }
     }
